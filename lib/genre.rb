e3a4de50b319feee1a702d0ad6247815f5b05b4d
Marianne Pyon <mariannepyon@gmail.com>
2019-09-04 16:51:14 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,28 @@
+require 'pry'
+class Genre
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def songs
+    Song.all.select do |songs|
+      songs.genre == self
+    end
+  end
+
+  def artists
+    Artist.all.select do |artists|
+      binding.pry
+      artists.genre == self
+    end
+  end
+
+  def self.all
+    @@all
+  end
+end
